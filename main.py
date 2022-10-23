e76997dc2416857aaabe5233a968672829dc8dba
Martin andersen[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[C <martinandersen3d@gmail.com>
2022-10-23 12:43:26 +0200

    init

---
+++ main.py
@@ -0,0 +1,66 @@
+
+
+from dataclasses import replace
+import os
+
+
+files = [
+    "C:\Github\Python\FileSearchReplace\TestFiles\CreateHelloWorldController.txt",
+    "C:\Github\Python\FileSearchReplace\TestFiles\Subdir\HelloWorldAPI.txt",
+]
+
+searchWord="HelloWorld"
+ReplaceWords=[
+    "Product",
+    "Order",
+    "ProductOrder",
+    "User",
+]
+
+def slash():
+    if ostype() == 'linux':
+        return '/'
+    if ostype() == 'win32':
+        return '\\'
+       
+def ostype():
+    import sys
+    if sys.platform.startswith('linux'):
+        # Linux specific procedures
+        return 'linux'
+    if sys.platform.startswith('darwin'):
+        # MacOs specific procedures
+        return 'darwin'
+    if sys.platform.startswith('win32'):
+        # Windows specific procedures
+        return 'win32'
+
+
+def createNewFile(oldPath, NewPath, oldWord,newWord):
+
+    try:
+
+        file = open(oldPath, "r")
+        content = file.read()
+        content = content.replace(oldWord, newWord)
+        
+        file2 = open(NewPath,"w") 
+        file2.write(content)
+
+    except Exception as e:
+        print("[ERROR]:",e)
+    finally:
+        file.close()    
+        file2.close()    
+
+for path in files:
+    oldPath = path
+    dirname = os.path.dirname(path)
+    #basename with extension
+    basename = os.path.basename(path)
+    print(dirname, basename)
+    for word in ReplaceWords:
+        newFilbaseename = basename.replace(searchWord, word)
+        newFilePath = f"{dirname}{slash()}{newFilbaseename}"
+        print(newFilePath)
+        createNewFile(oldPath, newFilePath, searchWord, word)
